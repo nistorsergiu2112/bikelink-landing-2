ffe4ff970af73db305dc6405251205f0e8bf703c
Sergiu Nistor Cosmin <nistorsergiu2112@gmail.com>
2025-07-24 02:09:33 +0300

    fix mobile responsiveness

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -69,13 +69,8 @@ function Header({
           onMouseEnter={() => setLogoHovered(true)}
           onMouseLeave={() => setLogoHovered(false)}
         >
-          <Logomark
-            className="h-8 sm:hidden"
-            invert={invert}
-            filled={logoHovered}
-          />
           <Logo
-            className="hidden h-8 sm:block"
+            className="h-8"
             invert={invert}
             filled={logoHovered}
           />
